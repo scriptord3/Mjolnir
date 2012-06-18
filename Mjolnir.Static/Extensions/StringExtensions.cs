b1281be2b9ddab5c7f7569de8b163291cadfeeff
scriptor <scriptor@aegisdev.eu>
2012-06-18 11:35:52 +0200

    Added string extension to decrypt/encrypt text

---
+++ Mjolnir.Static/Extensions/StringExtensions.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Mjolnir.Static.Extensions
+{
+    public static class StringExtensions
+    {
+        private static byte[] _salt = System.Text.Encoding.ASCII.GetBytes("o6806642kbM7c5");
+
+        public static string Decrypt(this string text, string secret)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException("text");
+            }
+            if (string.IsNullOrEmpty(secret))
+            {
+                throw new ArgumentNullException("secret");
+            }
+
+            // Declare the RijndaelManaged object
+            // used to decrypt the data.
+            RijndaelManaged aesAlg = null;
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            try
+            {
+                // generate the key from the shared secret and the salt
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(secret, _salt);
+
+                // Create a RijndaelManaged object
+                // with the specified key and IV.
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(Convert.ToInt32(aesAlg.KeySize / 8));
+                aesAlg.IV = key.GetBytes(Convert.ToInt32(aesAlg.BlockSize / 8));
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                // Create the streams used for decryption.                
+                byte[] bytes = Convert.FromBase64String(text);
+                using (System.IO.MemoryStream msDecrypt = new System.IO.MemoryStream(bytes))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (System.IO.StreamReader srDecrypt = new System.IO.StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                {
+                    aesAlg.Clear();
+                }
+            }
+
+            return plaintext;
+        }
+
+        public static string Encrypt(this string text, string secret)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException("text is empty");
+            }
+            if (string.IsNullOrEmpty(secret))
+            {
+                throw new ArgumentNullException("secret is empty");
+            }
+
+            string outStr = null;
+            // Encrypted string to return
+            RijndaelManaged aesAlg = null;
+            // RijndaelManaged object used to encrypt the data.
+            try
+            {
+                // generate the key from the shared secret and the salt
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(secret, _salt);
+
+                // Create a RijndaelManaged object
+                // with the specified key and IV.
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(Convert.ToInt32(aesAlg.KeySize / 8));
+                aesAlg.IV = key.GetBytes(Convert.ToInt32(aesAlg.BlockSize / 8));
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (System.IO.MemoryStream msEncrypt = new System.IO.MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (System.IO.StreamWriter swEncrypt = new System.IO.StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(text);
+                        }
+                    }
+                    outStr = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+            finally
+            {
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                {
+                    aesAlg.Clear();
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return outStr;
+
+        }
+    }
+}
